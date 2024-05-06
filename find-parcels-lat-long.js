cf9f0672cbaaa3f24193622dd63cc26bc2e42ffa
Alexandru Munteanu <m@ioalex.net>
2024-05-06 07:57:06 +0200

    ⚡️ Improve performances of `find-parcels-lat-long` by using 8 worker threads - still experimental

--- find-parcels-lat-long.js
+++ find-parcels-lat-long.js
@@ -1,10 +1,6 @@
 const csv = require('csv-parser')
 const {createReadStream} = require('fs')
-const {getParcellesDepartement} = require("./lib/parcelles")
-const {getLonLat} = require("./lib/turf")
-const fs = require('fs')
-
-const DATE_AND_DEPARTMENT_SEPARATOR = "&"
+const {Worker} = require('worker_threads');
 
 const readParcelsMatchesByDateAndDepartment = () => {
     const parcelsByDateAndDepartment = {}
@@ -32,40 +28,46 @@ const readParcelsMatchesByDateAndDepartment = () => {
     })
 }
 
+function splitToNChunks(array, n) {
+    let result = [];
+    for (let i = n; i > 0; i--) {
+        result.push(array.splice(0, Math.ceil(array.length / i)));
+    }
+    return result;
+}
+
+const processOne = (parcelsByDateAndDepartment, dateAndDepartmentArray) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker("./lat-long-worker.js", {
+            workerData: {
+                parcelsByDateAndDepartment,
+                dateAndDepartmentArray
+            }
+        })
+
+        worker.on('message', _ => {
+            return resolve
+        })
+        worker.on('error', reject)
+        worker.on('exit', code => {
+            if (code !== 0) {
+                reject(new Error(`Worker exited; code ${code}`))
+            }
+        })
+    });
+}
+
+const processInParallel = (parcelsByDateAndDepartment, dateAndDepartmentArrays) => {
+    return dateAndDepartmentArrays.map((dateAndDepartmentArray) => {
+        return processOne(parcelsByDateAndDepartment, dateAndDepartmentArray);
+    });
+}
+
 const main = async () => {
     const parcelsByDateAndDepartment = await readParcelsMatchesByDateAndDepartment()
+    const dateAndDepartmentArrays = splitToNChunks(Object.keys(parcelsByDateAndDepartment), 8);
 
-    for (const dateAndDepartment in parcelsByDateAndDepartment) {
-        const dateAndDepartmentArray = dateAndDepartment.split(DATE_AND_DEPARTMENT_SEPARATOR)
-        const date = dateAndDepartmentArray[0]
-        const department = dateAndDepartmentArray[1]
-
-        console.log(`${new Date().getTime() / 1000}: Récupération des parcelles du millesime ${date} et département ${department}`)
-
-        const parcelsToDecorate = parcelsByDateAndDepartment[dateAndDepartment]
-
-        const parcelIdsToFind = parcelsToDecorate.map((p) => p.parcel)
-
-        const parcelles = await getParcellesDepartement(department, date, parcelIdsToFind)
-
-        console.log('Récupération des latitudes et longitudes...')
-
-        for (const parcelToDecorate of parcelsToDecorate) {
-            const parcelle = parcelles[parcelToDecorate.parcel]
-            const [lon, lat] = getLonLat(parcelle)
-            parcelToDecorate.longitude = lon
-            parcelToDecorate.latitude = lat
-        }
-
-        const fsWriteStream = fs.createWriteStream('parcels-matches-lon-lat.csv', {flags: 'a'})
-        for (const decoratedParcel of parcelsToDecorate) {
-            //TODO: dummy CSV - a CSV lib would probably be better
-            const csv = decoratedParcel.parcel + ',' + decoratedParcel.date + ',' + decoratedParcel.department +
-                ',' + decoratedParcel.longitude + ',' + decoratedParcel.latitude + '\n'
-            fsWriteStream.write(csv)
-        }
-        fsWriteStream.end('')
-    }
+    await processInParallel(parcelsByDateAndDepartment, dateAndDepartmentArrays);
 }
 
 main().catch(error => {
---
+++ lat-long-worker.js
@@ -0,0 +1,43 @@
+const {getParcellesDepartement} = require("./lib/parcelles");
+const {getLonLat} = require("./lib/turf");
+const fs = require("fs");
+const workerData = require('node:worker_threads').workerData
+
+const process = async () => {
+    const dateAndDepartmentArray = workerData.dateAndDepartmentArray;
+    const parcelsByDateAndDepartment = workerData.parcelsByDateAndDepartment;
+
+    for (const dateAndDepartment of dateAndDepartmentArray) {
+        const dateAndDepartmentParts = dateAndDepartment.split("&")
+        const date = dateAndDepartmentParts[0]
+        const department = dateAndDepartmentParts[1]
+
+        console.log(`${new Date().getTime() / 1000}: Récupération des parcelles du millesime ${date} et département ${department}`)
+
+        const parcelsToDecorate = parcelsByDateAndDepartment[dateAndDepartment]
+
+        const parcelIdsToFind = parcelsToDecorate.map((p) => p.parcel)
+
+        const parcelles = await getParcellesDepartement(department, date, parcelIdsToFind)
+
+        console.log('Récupération des latitudes et longitudes...')
+
+        for (const parcelToDecorate of parcelsToDecorate) {
+            const parcelle = parcelles[parcelToDecorate.parcel]
+            const [lon, lat] = getLonLat(parcelle)
+            parcelToDecorate.longitude = lon
+            parcelToDecorate.latitude = lat
+        }
+
+        const fsWriteStream = fs.createWriteStream('parcels-matches-lon-lat.csv', {flags: 'a'})
+        for (const decoratedParcel of parcelsToDecorate) {
+            //TODO: dummy CSV - a CSV lib would probably be better
+            const csv = decoratedParcel.parcel + ',' + decoratedParcel.date + ',' + decoratedParcel.department +
+                ',' + decoratedParcel.longitude + ',' + decoratedParcel.latitude + '\n'
+            fsWriteStream.write(csv)
+        }
+        fsWriteStream.end('')
+    }
+}
+
+process();
